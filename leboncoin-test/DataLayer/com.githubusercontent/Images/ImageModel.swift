119dded14ecb5d94a7703252ec3906b6f237fa3a
Mathis Detourbet <mathis.detourbet@gmail.com>
2020-10-13 12:26:27 +0200

    [+] Make ImageModel decodable

--- leboncoin-test/DataLayer/com.githubusercontent/Images/ImageModel.swift
+++ leboncoin-test/DataLayer/com.githubusercontent/Images/ImageModel.swift
@@ -11,3 +11,15 @@ struct ImageModel {
     let small: String
     let thumb: String
 }
+
+extension ImageModel: Decodable {
+    enum ImageModelKeys: String, CodingKey {
+        case small, thumb
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ImageModelKeys.self)
+        self.small = try container.decode(String.self, forKey: .small)
+        self.thumb = try container.decode(String.self, forKey: .thumb)
+    }
+}
