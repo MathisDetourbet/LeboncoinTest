a18c3fd57a2c3d86d8489d786d39e30631d1529d
Mathis Detourbet <mathis.detourbet@gmail.com>
2020-10-14 18:42:26 +0200

    [+] add VC + dataAccess

---
+++ leboncoin-test/Features/AdvertisementList/AdvertisementListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AdvertisementListViewController.swift
+//  leboncoin-test
+//
+//  Created by Mathis Detourbet on 14/10/20.
+//
+
+import UIKit
+
+final class AdvertisementListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- leboncoin-test/RootCoordinator.swift
+++ leboncoin-test/RootCoordinator.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 final class RootCoordinator {
-    
     private let navigationController: UINavigationController
+    
+    // MARK: Dependency property to inject
+    private let httpService: HTTPService
+    private let httpConfiguration: HTTPConfiguration
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
+        self.httpConfiguration = HTTPConfiguration()
+        self.httpService = HTTPService()
     }
     
     func start() {
+        let dataAccess = HTTPAdvsertisementsListDataAccessor(httpService: httpService, httpConfiguration: httpConfiguration)
         
     }
 }
